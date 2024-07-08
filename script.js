db8b203aefe0ff9b11f2646c34255b93dc6fc638
dante0518 <90151246+dante0518@users.noreply.github.com>
2024-07-09 01:22:44 +0200

    added a loading screen

--- script.js
+++ script.js
@@ -28,8 +28,38 @@ function handleScroll() {
         window.removeEventListener('scroll', handleScroll);
     }
 }
-
+window.onbeforeunload = () => {window.scrollTo(0,0)};
 window.addEventListener('scroll', handleScroll);
+
+const counter = document.querySelector('.counter');
+const loadingBar = document.querySelector('.loading-bar-front');
+const container = document.querySelector('.loading-container');
+
+let idx = 0;
+updatenum();
+
+function updatenum() {
+    counter.innerHTML = idx + "%";
+    loadingBar.style.width = idx + "%";
+    idx+=2;
+
+    if (idx <= 100) {
+        setTimeout(updatenum, 30);
+    } else {
+        console.log("loaded");
+        setTimeout(fadeOutContainer, 1000);
+    }
+}
+
+function fadeOutContainer() {
+    container.classList.add('fade-out');
+    setTimeout(hideLoading, 1000); 
+}
+
+function hideLoading() {
+    container.style.display = "none";
+    console.log("Container hidden");
+}
 
 
 
